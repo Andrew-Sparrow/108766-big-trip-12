46c57c6915c2f24f7a99169f952c3379bb6c8e8e
Andrew <8911102@gmail.com>
2020-07-30 20:59:37 +0500

    trip-info container was added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 'use strict';
 
-let header = document.querySelector('.page-header');
-let headerControls = header.querySelector('.trip-main');
-let tripViewTitle = headerControls.querySelector('.trip-view');
+let header = document.querySelector(`.page-header`);
+let tripMain = header.querySelector(`.trip-main`);
+let tripViewTitle = tripMain.querySelector(`.trip-view`);
 
-let menu = () => {
-  return (``);
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
 };
+
+let tripInfoContainer = () => {
+  return (`<section class="trip-main__trip-info  trip-info">
+            <div class="trip-info__main">
+              <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
+
+              <p class="trip-info__dates">Mar 18&nbsp;—&nbsp;20</p>
+            </div>
+
+            <p class="trip-info__cost">
+              Total: €&nbsp;<span class="trip-info__cost-value">1230</span>
+            </p>
+          </section>`);
+};
+
+render(tripMain, tripInfoContainer(), `afterbegin`);
