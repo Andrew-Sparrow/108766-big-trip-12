90f00dc58575680f50a079084b5281765f625c0e
Andrew <8911102@gmail.com>
2020-07-30 14:53:05 +0500

    .idea folder was added to .gitignore

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let header = document.querySelector('.page-header');
+let headerControls = header.querySelector('.trip-main');
+let tripViewTitle = headerControls.querySelector('.trip-view');
+
+let menu = () => {
+  return (``);
+};
