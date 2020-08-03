ab7af59d50160d24baf916dcc69e53bfb239782e
Andrew <8911102@gmail.com>
2020-08-03 19:16:45 +0500

    file event-offer-in-event-item.js was added

---
+++ src/view/event-offer-in-event-item.js
@@ -0,0 +1,5 @@
+export const getEventOffer = () => {
+  return (`<li class="event__offer">
+            <span class="event__offer-title">Order Uber</span>€&nbsp;<span class="event__offer-price">20</span>
+           </li>`);
+};
