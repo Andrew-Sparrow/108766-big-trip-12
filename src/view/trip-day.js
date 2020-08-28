ccfb17948a26d3759660efa3553f4f7a8376a8b7
Andrew <8911102@gmail.com>
2020-08-28 09:10:01 +0500

    refactoring some variables were renamed

--- src/view/trip-day.js
+++ src/view/trip-day.js
@@ -1,8 +1,8 @@
 import {createDOMElement} from "./util/utils.js";
 
 const FIRST_EVENT = 0;
-const SECOND_EVENT = 1;
-const THIRD_EVENT = 2;
+const MONTH_OF_EVENT = 1;
+const DAY_OF_EVENT = 2;
 
 const createEventDayTemplate = (day, index) => {
   const [
@@ -14,7 +14,7 @@ const createEventDayTemplate = (day, index) => {
   return (`<li class="trip-days__item  day">
              <div class="day__info">
                <span class="day__counter">${index + 1}</span>
-               <time class="day__date" datetime="${tripEventsInDay[FIRST_EVENT].dateStart.toISOString()}">${date.split(` `)[SECOND_EVENT]} ${date.split(` `)[THIRD_EVENT]}</time>
+               <time class="day__date" datetime="${tripEventsInDay[FIRST_EVENT].dateStart.toISOString()}">${date.split(` `)[MONTH_OF_EVENT]} ${date.split(` `)[DAY_OF_EVENT]}</time>
              </div>
              <!-- place for list of events in day-->
            </li>`);
