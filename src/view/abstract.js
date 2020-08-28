e0cb0930151b61a18142f22923d0427ccc26b3fb
Andrew <8911102@gmail.com>
2020-08-28 21:54:35 +0500

    5.1.2 Выносим общие методы в абстрактный класс

---
+++ src/view/abstract.js
@@ -0,0 +1,27 @@
+import {createDOMElement} from "./util/utils.js";
+
+export default class Abstract {
+  constructor() {
+    if (new.target === Abstract) {
+      throw new Error(`Can't instantiate Abstract, only concrete one.`);
+    }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method not implemented: getTemplate`);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createDOMElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
